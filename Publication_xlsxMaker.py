0999c3b987b20725bd704d2823e1060ec12f5f37
NasHas <92371754+NasHas@users.noreply.github.com>
2025-10-05 12:38:00 +0200

    Update Publication_xlsxMaker.py

--- Publication_xlsxMaker.py
+++ Publication_xlsxMaker.py
@@ -1,6 +1,6 @@
 '"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""'
-'### The following script is made as part of the study #Transfer learning of a pretrained preclinical neural network for robotic surgical assessment on limited clinical data###'
-'### The code is open-source. However, when using the code, please make a reference to our paper and repository.""""""""""""""""""""""""""""""""""""""""""""""""""""""'
+'### The following script is made as part of the Hashemi et al. papers regarding robot surgery and AI###'""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+'### The code is open-source. However, when using the code, please make a reference to our papers and repositories.""""""""""""""""""""""""""""""""""""""""""""""""""""
 '"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""'
 
 
@@ -25,3 +25,4 @@ for file in tsv_files:
 
     # Write the DataFrame to an XLSX file
     df.to_excel(output_path, index=False)
+
